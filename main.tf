d599577b60b0c4f241c98308a7273bec6afe8cb7
emergencydevops-collab <emergencydevops@gmail.com>
2024-09-05 07:51:20 +0530

    node number

--- main.tf
+++ main.tf
@@ -103,7 +103,7 @@ resource "aws_eks_node_group" "eks_node_group" {
   subnet_ids      = data.aws_subnets.public.ids
 
   scaling_config {
-    desired_size = 1
+    desired_size = 2
     max_size     = 2
     min_size     = 1
   }
