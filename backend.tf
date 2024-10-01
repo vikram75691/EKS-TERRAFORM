6391f51e5af26d3d0fdb473cc01e42a1f49d0dd7
emergencydevops-collab <emergencydevops@gmail.com>
2024-10-01 21:49:11 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "rajinikanths32024" # Replace with your actual S3 bucket name
+    bucket = "rajinikanths32024tf" # Replace with your actual S3 bucket name
     key    = "Eks/terraform.tfstate"
     region = "ap-south-1"
   }
